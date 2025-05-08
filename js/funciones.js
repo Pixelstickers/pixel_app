e5785c3582eb686dd3f64df44fd55c61774cc694
Pixelstickers <pixel.stickersss@gmail.com>
2025-05-08 12:34:03 -0300

    flechita

--- js/funciones.js
+++ js/funciones.js
@@ -391,20 +391,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Render inicial
     renderizarProductos();
         });
-        function toggleMenu() {
-            const nav = document.querySelector('.main-nav ul');
-            nav.classList.toggle('show');
-        }
         const btnVolverArriba = document.getElementById("btnVolverArriba");
 
         window.addEventListener("scroll", () => {
-        if (window.scrollY > 300) {
+          if (window.scrollY > 200) {
             btnVolverArriba.style.display = "block";
-        } else {
+          } else {
             btnVolverArriba.style.display = "none";
-        }
+          }
         });
-
+        
         btnVolverArriba.addEventListener("click", () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
+          window.scrollTo({ top: 0, behavior: "smooth" });
         });
+        
