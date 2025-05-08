2ead5492a01cc15f8d56f9c0d07c5f43e0a3d50a
Pixelstickers <pixel.stickersss@gmail.com>
2025-05-08 11:52:20 -0300

    flecha scroll

--- js/funciones.js
+++ js/funciones.js
@@ -395,4 +395,16 @@ function toggleMenu() {
     const nav = document.querySelector('.main-nav ul');
     nav.classList.toggle('show');
   }
-  
+  const btnVolverArriba = document.getElementById("btnVolverArriba");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnVolverArriba.style.display = "block";
+  } else {
+    btnVolverArriba.style.display = "none";
+  }
+});
+
+btnVolverArriba.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
